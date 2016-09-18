a3efeb9213f1776d25755189270d7a54283b72ee
U-Hugin\myrlyn <jonathon.l.walker@gmail.com>
2016-09-17 21:34:52 -0400

    java

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+//ignore this, not 100% necessary, just good practice
+package hello;
+//declare a new class
+public class HelloWorld
+{
+    //'main' is the method that is executed by default when running a java program.
+    //This is where coe execution begins
+    public static void main(String[] args)
+    {
+        //System.out.println is the method to print to a console/terminal.
+        System.out.println("Hello World!");
+
+    }
+
+}
